46ea57c91d2d00cf402f58f173e1a88865e36863
Ana Paula Pedroso <pedroso.anapaula@gmail.com>
2024-09-11 21:41:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+   {    titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Ela já coleciona títulos e patrocinadores, sendo um dos maiores nomes do skate mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada SK8"
+   },
+
+    {   titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Nascida em Guarulhos, ela superou diversas dificuldades. Com suas acrobacias impressionantes e sorriso contagiante, Rebeca já conquistou diversas medalhas em competições nacionais e internacionais, incluindo o ouro olímpico. Sua história inspiradora de superação e sucesso a tornou um símbolo de esperança e orgulho para o Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "judô judoca judo"
+    },
+      
+     {  titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou o ouro nas Olimpíadas de 2024. Sua técnica impecável e determinação a tornaram um ícone do esporte nacional. Com sua história inspiradora, ela motiva jovens atletas a perseguirem seus sonhos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ginastica ouro"
+     }
+     
+];
